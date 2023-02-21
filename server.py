75528c262194e87d2fc065d726ce56662f32df21
InFinity-dev <hypercube0702@gmail.com>
2023-02-21 23:51:40 +0900

    server-client poc

--- server.py
+++ server.py
@@ -1,42 +1,37 @@
-from flask import Flask, render_template, request
-from flask_socketio import SocketIO, emit, join_room
+from flask import Flask, render_template
+from flask_socketio import SocketIO, emit, join_room, leave_room, close_room, rooms, disconnect
+import random
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
-socketio = SocketIO(app)
+socketio = SocketIO(app, cors_allowed_origins='*')
 
-# List of users waiting for a match
-waiting_users = []
+# Store connected clients
+clients = set()
 
-@app.route('/')
-def index():
-    return render_template('index.html')
+# Store waiting clients
+waiting_clients = set()
 
 @socketio.on('connect')
-def on_connect():
-    # Add the user to the waiting list
-    waiting_users.append(request.sid)
-    print(f'User {request.sid} connected and added to the waiting list')
+def connect():
+    clients.add(request.sid)
 
-    # Check if there's a user waiting to be matched
-    if len(waiting_users) >= 2:
-        # Get the first two users from the waiting list
-        user1 = waiting_users.pop(0)
-        user2 = waiting_users.pop(0)
+@socketio.on('disconnect')
+def disconnect():
+    clients.remove(request.sid)
+    waiting_clients.discard(request.sid)
 
-        # Join the two users to a room with a unique name
-        room = f'{user1}-{user2}'
-        join_room(room)
-
-        # Notify the users that they have been matched and provide them with the room name
-        emit('matched', {'room': room}, to=user1)
-        emit('matched', {'room': room}, to=user2)
-
-@socketio.on('message')
-def on_message(data):
-    # Send the message to the other user in the same room
-    room = request.sid.split('-')[0] + '-' + request.sid.split('-')[1]
-    emit('message', data, room=room)
+@socketio.on('join_queue')
+def join_queue():
+    waiting_clients.add(request.sid)
+    if len(waiting_clients) > 1:
+        # Match two random clients
+        client1, client2 = random.sample(waiting_clients, 2)
+        waiting_clients.discard(client1)
+        waiting_clients.discard(client2)
+        # Send match event to clients
+        emit('match', {'client1': client1, 'client2': client2}, room=client1)
+        emit('match', {'client1': client1, 'client2': client2}, room=client2)
 
 if __name__ == "__main__":
     socketio.run(app, host='0.0.0.0', port=7777, debug=True)
