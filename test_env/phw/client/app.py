6b55636c1697ef3ec1754a37dab2df33e4151453
InFinity-dev <hypercube0702@gmail.com>
2023-02-22 07:16:57 +0900

    fixed spaghetti

--- test_env/phw/client/app.py
+++ test_env/phw/client/app.py
@@ -180,18 +180,9 @@ class SnakeGameClass:
 game = SnakeGameClass("./static/food.png")
 ######################################################################################
 
-waiting_players = []
-room_of_players = {}
-players_in_room = {} 
-last_created_room = ""
-
 @app.route("/", methods=["GET", "POST"])
 def index():
     return render_template("index.html")
-
-@app.route("/test", methods=["GET", "POST"])
-def test():
-    return render_template("test.html")
 
 @app.route("/enter_snake", methods=["GET", "POST"])
 def enter_snake():
@@ -204,53 +195,6 @@ def test_connect():
 @socketio.on('disconnect')
 def test_disconnect():
     print('Client disconnected')
-
-@app.route('/data')
-def data():
-    while True:
-        time.sleep(1)
-        socketio.emit('data', {'data': 'This is a data stream!'})
-
-
-# Handle join event
-@socketio.on('join')
-def handle_join():
-    sid = request.sid
-    global last_created_room
-    if len(waiting_players) == 0:
-        waiting_players.append(sid)
-        last_created_room = str(uuid.uuid4())
-
-        # register sid to the room
-        join_room(last_created_room)
-        room_of_players[sid] = last_created_room
-        emit('waiting', {'room_id' : last_created_room, 'sid' : sid}, to=last_created_room)
-    else:
-        host_sid = waiting_players.pop()
-        room_id = room_of_players[host_sid]
-        join_room(room_id)
-
-        sid = request.sid
-        room_of_players[sid] = room_id
-        
-        last_created_room = ""
-        print(room_of_players)
-        emit('matched', {'room_id' : room_id, 'sid' : sid}, to=room_id, broadcast=True)
-        emit('start-game', to=room_id)
-        
-
-# 소켓 테스트용 1초마다 시간 쏴주는 함수
-@app.route("/servertime")
-def servertime():
-    return render_template("servertime.html")
-
-
-@socketio.on('get_time')
-def get_time():
-    while True:
-        current_time = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-        socketio.emit('time', {'time': current_time})
-        socketio.sleep(1)
 
 @app.route('/snake')
 def snake():
