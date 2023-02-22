b7c8d90f79e7fea9affd5ea7786f7baa3f7e6f38
InFinity-dev <hypercube0702@gmail.com>
2023-02-23 01:26:11 +0900

    main update major

--- app.py
+++ app.py
@@ -18,6 +18,7 @@ import jinja2
 import aiohttp_jinja2
 import uuid
 from engineio.payload import Payload
+
 Payload.max_decode_packets = 200
 
 app = Flask(__name__)
@@ -26,7 +27,6 @@ app.config['SECRET_KEY'] = "roomfitisdead"
 socketio = SocketIO(app, cors_allowed_origins='*')
 
 ############################## SNAKE GAME LOGIC SECTION ##############################
-
 cap = cv2.VideoCapture(0)
 cap.set(3, 1280)
 cap.set(4, 720)
@@ -41,9 +41,9 @@ class SnakeGameClass:
         self.allowedLength = 150  # total allowed Length
         self.previousHead = 600, 350  # previous head point => random 값으로 주기
 
-        self.speed=0.1
-        self.velocityX=random.choice([-1,0,1])
-        self.velocityY=random.choice([-1,0,1])
+        self.speed = 0.1
+        self.velocityX = random.choice([-1, 0, 1])
+        self.velocityY = random.choice([-1, 0, 1])
 
         self.imgFood = cv2.imread(pathFood, cv2.IMREAD_UNCHANGED)
         self.hFood, self.wFood, _ = self.imgFood.shape
@@ -53,7 +53,7 @@ class SnakeGameClass:
         self.score = 0
         self.gameOver = False
 
-    #---collision function---
+    # ---collision function---
     def ccw(self, p, a, b):
         vect_sub_ap = [a[0] - p[0], a[1] - p[1]]
         vect_sub_bp = [b[0] - p[0], b[1] - p[1]]
@@ -83,7 +83,7 @@ class SnakeGameClass:
                 print(u2_pt)
                 return True
         return False
-    #---collision function---end
+    # ---collision function---end
 
     def randomFoodLocation(self):
         self.foodPoint = random.randint(100, 1000), random.randint(100, 600)
@@ -115,37 +115,37 @@ class SnakeGameClass:
 
     def my_snake_update(self, HandPoints, o_bodys):
         px, py = self.previousHead
-        #----HandsPoint moving ----
-        s_speed=30
+        # ----HandsPoint moving ----
+        s_speed = 30
         if HandPoints:
-            m_x,m_y=HandPoints
-            dx=m_x-px #-1~1
-            dy=m_y-py
+            m_x, m_y = HandPoints
+            dx = m_x - px  # -1~1
+            dy = m_y - py
 
-            #speed 범위: 0~1460
-            if math.hypot(dx, dy) > math.hypot(1280, 720)/10:
-                self.speed=math.hypot(1280, 720)/10 #146
+            # speed 범위: 0~1460
+            if math.hypot(dx, dy) > math.hypot(1280, 720) / 10:
+                self.speed = math.hypot(1280, 720) / 10  # 146
             elif math.hypot(dx, dy) < s_speed:
-                self.speed=s_speed
+                self.speed = s_speed
             else:
-                self.speed=math.hypot(dx, dy)
+                self.speed = math.hypot(dx, dy)
 
-            if dx!=0:
-                self.velocityX=dx/1280
-            if dy!=0:
-                self.velocityY=dy/720
+            if dx != 0:
+                self.velocityX = dx / 1280
+            if dy != 0:
+                self.velocityY = dy / 720
 
             # print(self.velocityX)
             # print(self.velocityY)
 
-            cx=round(px+self.velocityX*self.speed)
-            cy=round(py+self.velocityY*self.speed)
+            cx = round(px + self.velocityX * self.speed)
+            cy = round(py + self.velocityY * self.speed)
 
         else:
-            self.speed=s_speed
-            cx=round(px+self.velocityX*self.speed)
-            cy=round(py+self.velocityY*self.speed)
-        #----HandsPoint moving ----end
+            self.speed = s_speed
+            cx = round(px + self.velocityX * self.speed)
+            cy = round(py + self.velocityY * self.speed)
+        # ----HandsPoint moving ----end
 
         # print(f'{cx} , {cy}')
 
@@ -193,8 +193,6 @@ class SnakeGameClass:
             self.randomFoodLocation()
 
     def update(self, imgMain, receive_Data, HandPoints=[]):
-        o_body_node=receive_Data["opp_body_node"]
-        o_score=receive_Data["opp_score"]
 
         if self.gameOver:
             # pass
@@ -204,38 +202,50 @@ class SnakeGameClass:
                                scale=7, thickness=5, offset=20)
         else:
             # draw others snake
-            imgMain=self.draw_snakes(imgMain, o_body_node, o_score)
+            o_body_node = []
+            o_score = 0
+
+            if receive_Data:
+                o_body_node = receive_Data["opp_body_node"]
+                o_score = receive_Data["opp_score"]
+
+            imgMain = self.draw_snakes(imgMain, o_body_node, o_score)
 
             # update and draw own snake
             self.my_snake_update(HandPoints, o_body_node)
-            imgMain=self.draw_Food(imgMain)
-            imgMain=self.draw_snakes(imgMain, self.points, self.score)
+            imgMain = self.draw_Food(imgMain)
+            imgMain = self.draw_snakes(imgMain, self.points, self.score)
 
         return imgMain
 
+
 game = SnakeGameClass("./static/food.png")
 
-opponent_data=[]
+opponent_data = []
 ######################################################################################
 
 @app.route("/", methods=["GET", "POST"])
 def index():
     return render_template("index.html")
 
+
 @app.route("/enter_snake", methods=["GET", "POST"])
 def enter_snake():
     room_id = request.args.get('room_id')
     sid = request.args.get('sid')
     print(room_id, sid)
-    return render_template("snake.html", room_id = room_id, sid = sid)
+    return render_template("snake.html", room_id=room_id, sid=sid)
+
 
 @socketio.on('connect')
 def test_connect():
     print('Client connected')
 
+
 @socketio.on('disconnect')
 def test_disconnect():
     print('Client disconnected')
+
 
 @socketio.on('opp_data_transfer')
 def opp_data_transfer(data):
@@ -250,6 +260,7 @@ def opp_data_transfer(data):
     opponent_data = data['data']
     # socketio.emit('opp_data_to_test_server', {'data' : data}, broadcast=True)
     print('Received data from client:', opp_head_x, opp_head_y, opp_score, opp_sid)
+
 
 @app.route('/snake')
 def snake():
@@ -267,18 +278,15 @@ def snake():
                 lmList = hands[0]['lmList']
                 pointIndex = lmList[8][0:2]
 
-
-            img = game.update(img, pointIndex, opponent_data)
+            img = game.update(img, opponent_data, pointIndex)
 
             # encode the image as a JPEG string
             _, img_encoded = cv2.imencode('.jpg', img)
             yield (b'--frame\r\n'
                    b'Content-Type: image/jpeg\r\n\r\n' + img_encoded.tobytes() + b'\r\n')
 
-
     return Response(generate(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
 
 if __name__ == "__main__":
-    socketio.run(app, host='0.0.0.0', port=5000, debug=True)
-
+    socketio.run(app, host='localhost', port=5000, debug=True)
