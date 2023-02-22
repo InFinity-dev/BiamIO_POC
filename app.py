4f69c9b306d7d32c0e71ec6fb5129ecfedc6b7c2
phwGithub <yey6689@gmail.com>
2023-02-23 08:48:18 +0900

    redirect after gameover

--- app.py
+++ app.py
@@ -287,7 +287,7 @@ def snake():
         global opponent_data
         global game
         global gameover_flag
-        
+
         while True:
             success, img = cap.read()
             img = cv2.flip(img, 1)
@@ -310,9 +310,9 @@ def snake():
                 print("game ended")
                 gameover_flag = False
                 time.sleep(1)
+                socketio.emit('gameover')
+                time.sleep(2)
                 break
-            else:
-                pass
 
     return Response(generate(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
